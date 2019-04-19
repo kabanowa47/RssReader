47c8ec5143d33d0ba428722b2f3404d0d25704fa
kabanowa47 <kabanowa47@gmail.com>
2019-04-19 20:12:06 +0200

    now with extended libaries from feed40j package

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        RssEngine rssEngine = new RssEngine();
+        rssEngine.rssEngine("http://feeds.reuters.com/reuters/peopleNews");
+
+    }
+}
