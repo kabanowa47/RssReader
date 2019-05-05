42321220436eea2e94e2d7e697e0edebc1056604
kabanowa47 <kabanowa47@gmail.com>
2019-05-05 09:37:21 +0200

    maven project

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-
-        RssEngine rssEngine = new RssEngine();
-        rssEngine.rssEngine("http://feeds.reuters.com/reuters/peopleNews");
-// new line
-    }
-}
---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+
+    public class Main {
+
+        public static void main(String[] args) throws Exception {
+
+            RssEngine rssEngine = new RssEngine();
+            rssEngine.rssEngine("http://feeds.reuters.com/reuters/peopleNews");
+// new line
+        }
+    }
+
+
